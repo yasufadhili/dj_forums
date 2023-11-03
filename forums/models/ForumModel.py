2efbbf6168eeebe473ac9f3ef17660f163d586c7
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-03 11:21:01 +0300

    Update ForumModel.py

--- forums/models/ForumModel.py
+++ forums/models/ForumModel.py
@@ -22,12 +22,27 @@ class DateFields(Model):
 
 class Forum(DateFields):
 
+    ACCESSIBILITY_CHOICES = (
+        ('open', 'Open'),
+        ('subscribed', 'Subscribed')
+    )
+
+    POSTING_PERMISSIONS_CHOICES = (
+        #('creator', 'Creator'),
+        ('creator_managers', 'Creators & Managers'),
+        ('subscribers', 'Subscribers'),
+        ('everyone', 'Everyone'),
+    )
+
     author = ForeignKey(User, on_delete=CASCADE)
     title = CharField(max_length=255)
     slug = SlugField(unique=True, blank=True)
     image = URLField(blank=True, max_length=3000)
     description = TextField(max_length=1000, blank=True)
-    moderators = ManyToManyField(User, verbose_name=_("Forum moderators"))
+    managers  = ManyToManyField(User, verbose_name=_("Forum managers"))
+    accessibility = CharField(max_length=50, choices=ACCESSIBILITY_CHOICES, default='open')
+    posting_permissions = CharField(max_length=50, choices=POSTING_PERMISSIONS_CHOICES, default='everyone')
+    subscribers = ManyToManyField(User, related_name='subscribed_forums', default='creator_managers', blank=True)
 
     class Meta:
         verbose_name = _("Forum")
@@ -38,5 +53,28 @@ class Forum(DateFields):
     
     def get_absolute_url(self):
         return reverse("Forum_detail", kwargs={"pk": self.pk})
+    
+    def can_user_post(self, user):
+        if self.accessibility == "subscribed":
+            return user in self.subscribers.all()
+        elif self.posting_permissions == 'everyone':
+            return True
+        else:
+            return False
+        
+    def can_user_post(self, user):
+        if self.accessibility == 'subscribed':
+            # If the forum is 'subscribed', check if the user is a subscriber
+            return user in self.subscribers.all()
+        elif self.posting_permissions == 'everyone':
+            # If the posting permissions are set to 'everyone', anyone can post
+            return True
+        elif self.posting_permissions == 'creator_managers':
+            # If the posting permissions are set to 'creator & managers',
+            # check if the user is the creator or a designated manager
+            return user == self.author or user in self.managers .all()
+        else:
+            return False  # Default to not allowing the user to post
+
 
 
