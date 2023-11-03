3e859a693a2caae28b4f263ebbcb759615838b7e
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-03 11:34:21 +0300

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
-from dj_rest_auth.registration.views import RegisterView
+#from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import (
     LoginView, LogoutView, UserDetailsView
 )
 
-from . import views
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="rest_register"),
+    #path("register/", RegisterView.as_view(), name="rest_register"),
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
     path("user/", UserDetailsView.as_view(), name="rest_user_details")
