7255b160f2be3f1d65c4b8e7b448666c7275e9b2
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-03 10:58:16 +0300

    Update requests.py

--- requests.py
+++ requests.py
@@ -38,3 +38,24 @@ def register():
     else:
         print("Registration failed. Check the API endpoint or data format.")
 
+
+def user_data():
+    
+    restricted_data_url = "http://localhost:8000/auth/user/" 
+
+    # Include the token in the request headers
+    headers = {
+        "Authorization": f"Token {'token'}",  
+    }
+
+    # Make a GET request with the token in the headers
+    response = requests.get(restricted_data_url, headers=headers)
+
+    # Check the response status code
+    if response.status_code == 200:
+        data = response.json()  # Access the restricted data in the response
+        print("Data retrieved successfully:", data)
+    else:
+        print("Failed to retrieve data. Check the API endpoint or authentication token.")
+
+
