194c206f9d110dfbb4a2acafcdd05bb9916c855d
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-03 13:19:31 +0300

    Update PostModel.py

---
+++ forums/models/PostModel.py
@@ -0,0 +1,44 @@
+
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
+from forums.models.ForumModel import *
+from forums.models.ThreadModel import Thread
+
+User = get_user_model()
+
+
+class Post(Model):
+
+    author = ForeignKey(User, on_delete=CASCADE, related_name="thread_posts")
+    thread = ForeignKey(Thread, on_delete=CASCADE)
+    content = TextField(max_length=1000)
+    views = PositiveIntegerField(default=0)
+    likes = PositiveIntegerField(default=0)
+    dislikes = PositiveIntegerField(default=0)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Thread Post")
+        verbose_name_plural = _("Thread Posts")
+
+    def __str__(self):
+        return f"{self.content[:50]}"
+    
+    def get_absolute_url(self):
+        return reverse("ThreadPost_detail", kwargs={"pk": self.pk})
+    
+    def increment_views(self):
+        self.views += 1
+        self.save()
+
+    def increment_likes(self):
+        self.likes += 1
+        self.save()
+
+    def increment_dislikes(self):
+        self.dislikes += 1
+        self.save()
+
+
