1ae81b112dea54efd6d8c9d1a81d8071def85b9a
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-03 08:43:39 +0300

    Created initlal forums app

--- dj_forums/settings.py
+++ dj_forums/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "authentication.apps.AuthenticationConfig",
+    "forums.apps.ForumsConfig",
 
     "rest_framework",
     "rest_framework.authtoken",
