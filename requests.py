bbd731ff8552211214f05a6eab7669e1ed448e82
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-03 10:56:06 +0300

    Create requests.py

---
+++ requests.py
@@ -0,0 +1,40 @@
+import requests
+
+def login():
+    login_url = "http://localhost:8000/auth/login/" 
+
+    # Create a dictionary with the user's credentials
+    login_data = {
+        "username": "user1",
+        "password": "userpass123"
+    }
+
+    # Make a POST request to the login endpoint
+    response = requests.post(login_url, data=login_data)
+
+    # Check the response status code
+    if response.status_code == 200:
+        print("Login successful!")
+        token = response.json().get("token")
+        print(token)
+    else:
+        print("Login failed. Check your credentials or the API endpoint.")
+
+def register():
+    register_url = "http://localhost:8000/auth/register/" 
+    
+    registration_data = {
+        "username": "new_user",
+        "password": "password123", 
+        "email": "new_user@example.com",  
+    }
+
+    # Make a POST request to the registration endpoint
+    response = requests.post(register_url, data=registration_data)
+    
+    # Check the response status code
+    if response.status_code == 201:
+        print("Registration successful!")
+    else:
+        print("Registration failed. Check the API endpoint or data format.")
+
