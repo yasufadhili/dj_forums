20ebfdb3d9077c906cad36464a3ed4a7adcdff2c
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-05 13:08:11 +0300

    Update CommentModel.py

--- forums/models/CommentModel.py
+++ forums/models/CommentModel.py
@@ -7,15 +7,13 @@ from forums.models.PostModel import Post
 User = get_user_model()
 
 
-class Comment(Model):
+class Comment(DateTimeModel):
 
     post = ForeignKey(Post, on_delete=CASCADE, related_name="thread_post_comments")
     content = TextField(max_length=1000)
     views = PositiveIntegerField(default=0)
     likes = PositiveIntegerField(default=0)
     dislikes = PositiveIntegerField(default=0)
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = _("Comment")
