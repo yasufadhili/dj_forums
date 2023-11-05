be8e423bf091419e1443a2d2bd5b01b2aa528d47
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-05 13:36:03 +0300

    Update EngagementModel.py

--- forums/models/EngagementModel.py
+++ forums/models/EngagementModel.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 from forums.models.ForumModel import *
 from forums.models.CommentModel import Comment
@@ -25,36 +27,45 @@ class Reply(DateTimeModel):
         return reverse("Reply_detail", kwargs={"pk": self.pk})
 
 
+VOTE_TYPES = (
+        ('T', 'Thread'),
+        ('P', 'Comment'),
+        ('C', 'Comment'),
+        ('R', 'Reply'),
+    )
 
-class Upvote(DateTimeModel):
+class UpVote(Model):
+    author = ForeignKey(User, on_delete=CASCADE)
+    created_at = DateTimeField(auto_now_add=True)
+    content_type = ForeignKey(ContentType, on_delete=CASCADE)
+    object_id = PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
-    author = ForeignKey(User, on_delete=CASCADE, related_name="upvotes")
-    comment = ForeignKey(Comment, on_delete=CASCADE)
-
+    upvote_type = CharField(max_length=1, choices=VOTE_TYPES)
+    
     class Meta:
         verbose_name = _("Upvote")
         verbose_name_plural = _("Upvotes")
+
+    def get_upvote_type(self):
+        return dict(VOTE_TYPES)[self.upvote_type]
+
+
+class DownVote(Model):
+    author = ForeignKey(User, on_delete=CASCADE)
+    created_at = DateTimeField(auto_now_add=True)
+    content_type = ForeignKey(ContentType, on_delete=CASCADE)
+    object_id = PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     
-    def __str__(self):
-        return self.author
-    
-    def get_absolute_url(self):
-        return reverse("Upvote_detail", kwargs={"pk": self.pk})
-
-
-class Downvote(Model):
-
-    author = ForeignKey(User, on_delete=CASCADE, related_name="downvotes")
+    upvote_type = CharField(max_length=1, choices=VOTE_TYPES)
 
     class Meta:
         verbose_name = _("Downvote")
         verbose_name_plural = _("Downvotes")
 
-    def __str__(self):
-        return self.author
-
-    def get_absolute_url(self):
-        return reverse("Downvote_detail", kwargs={"pk": self.pk})
+    def get_upvote_type(self):
+        return dict(VOTE_TYPES)[self.upvote_type]
 
 
 
