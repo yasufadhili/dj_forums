ac853e05e94e24e0ed2c98950be4a030e775a5a8
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-06 09:39:33 +0300

    Update CommentModel.py

--- forums/models/CommentModel.py
+++ forums/models/CommentModel.py
@@ -26,23 +26,25 @@ class Comment(Model):
 
     def get_absolute_url(self):
         return reverse("Comment_detail", kwargs={"pk": self.pk})
-    
+
+    '''
     def total_upvotes(self):
         return get_total_upvotes(self, self.id, 'C')
 
     def total_downvotes(self):
         return get_total_downvotes(self, self.id, 'C')
+    '''
     
     def increment_views(self):
         self.views += 1
         self.save()
 
-    def increment_likes(self):
-        self.likes += 1
-        self.save()
+    def total_comment_upvotes(self):
+        content_type = ContentType.objects.get_for_model(self)
+        return UpVote.objects.filter(content_type=content_type, upvote_type='C', object_id=self.id).count()
 
-    def increment_dislikes(self):
-        self.dislikes += 1
-        self.save()
+    def total_comment_downvotes(self):
+        content_type = ContentType.objects.get_for_model(self)
+        return DownVote.objects.filter(content_type=content_type, upvote_type='C', object_id=self.id).count()
 
 
