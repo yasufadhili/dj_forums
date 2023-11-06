c3c7b2627d3cc0b449bf407991839b32ae4296bf
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-06 12:10:50 +0300

    Update signals.py

--- forums/signals.py
+++ forums/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from forums.models.ForumModel import Forum
 
+
 @receiver(post_save, sender=Forum)
 def add_creator_to_managers(sender, instance, created, **kwargs):
     if created:
