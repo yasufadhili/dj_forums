9d1aef0e82a663c56bfc0dacc274f731644ce4b9
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-06 10:18:02 +0300

    Update EngagementModel.py

--- forums/models/EngagementModel.py
+++ forums/models/EngagementModel.py
@@ -1,86 +1,76 @@
+from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
-from forums.models.ForumModel import *
-from forums.models.CommentModel import Comment
-
 User = get_user_model()
 
 
-
-class Reply(Model):
-
-    author = ForeignKey(User, on_delete=CASCADE, related_name="upvotes")
-    comment = ForeignKey(Comment, on_delete=CASCADE)
-    content = TextField(max_length=1000)
+class Reply(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="upvotes")
+    comment = models.ForeignKey('forums.Comment', on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
 
     class Meta:
         verbose_name = _("Reply")
-        verbose_name_plural = _("Replys")
+        verbose_name_plural = _("Replies")
 
     def __str__(self):
         return self.content[:50]
 
     def get_absolute_url(self):
         return reverse("Reply_detail", kwargs={"pk": self.pk})
-    
-    def total_upvotes(self):
-        return get_total_upvotes(self, self.id, 'R')
-
-    def total_downvotes(self):
-        return get_total_downvotes(self, self.id, 'R')
 
 
-VOTE_TYPES = (
+class UpVote(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    VOTE_TYPES = (
         ('T', 'Thread'),
         ('P', 'Post'),
         ('C', 'Comment'),
         ('R', 'Reply'),
     )
 
-class UpVote(Model):
-    author = ForeignKey(User, on_delete=CASCADE)
-    created_at = DateTimeField(auto_now_add=True)
-    content_type = ForeignKey(ContentType, on_delete=CASCADE)
-    object_id = PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
-
-    upvote_type = CharField(max_length=1, choices=VOTE_TYPES)
+    upvote_type = models.CharField(max_length=1, choices=VOTE_TYPES)
 
     class Meta:
         verbose_name = _("Upvote")
         verbose_name_plural = _("Upvotes")
 
     def get_upvote_type(self):
-        return dict(VOTE_TYPES)[self.upvote_type]
+        return dict(self.VOTE_TYPES)[self.upvote_type]
 
 
-class DownVote(Model):
-    author = ForeignKey(User, on_delete=CASCADE)
-    created_at = DateTimeField(auto_now_add=True)
-    content_type = ForeignKey(ContentType, on_delete=CASCADE)
-    object_id = PositiveIntegerField()
+class DownVote(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
-    
-    upvote_type = CharField(max_length=1, choices=VOTE_TYPES)
+
+    upvote_type = models.CharField(max_length=1, choices=UpVote.VOTE_TYPES)
 
     class Meta:
         verbose_name = _("Downvote")
         verbose_name_plural = _("Downvotes")
 
     def get_upvote_type(self):
-        return dict(VOTE_TYPES)[self.upvote_type]
+        return dict(UpVote.VOTE_TYPES)[self.upvote_type]
 
 
-# utility function to get total upvotes and downvotes for a given model
+# Utility function to get total upvotes and downvotes for a given model
 def get_total_upvotes(model, object_id, upvote_type):
     content_type = ContentType.objects.get_for_model(model)
     return UpVote.objects.filter(content_type=content_type, object_id=object_id, upvote_type=upvote_type).count()
 
+
 def get_total_downvotes(model, object_id, upvote_type):
     content_type = ContentType.objects.get_for_model(model)
     return DownVote.objects.filter(content_type=content_type, object_id=object_id, upvote_type=upvote_type).count()
-
-
