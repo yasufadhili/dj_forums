48558ae1d705d6bd192020176d16d25b5e17439f
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-06 11:45:49 +0300

    Update urls.py

--- dj_forums/urls.py
+++ dj_forums/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("auth/", include("authentication.urls"))
+    path("auth/", include("authentication.urls")),
+    path("forums/", include("forums.urls"))
 ]
