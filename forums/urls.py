37fe7c2b77bc1fd8c66e9070d70772070f9cea9b
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-06 11:45:44 +0300

    Update urls.py

--- forums/urls.py
+++ forums/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
-from forums import views
+from rest_framework import urls, routers
+
+from forums.views.ForumView import ForumViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r"", ForumViewSet, basename="forum")
 
 urlpatterns = [
-    
+    path("", include(router.urls))
 ]
 
