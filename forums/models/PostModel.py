ae2999f41eb290fb99104a0adeb465f35c54e303
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-06 10:17:52 +0300

    Update PostModel.py

--- forums/models/PostModel.py
+++ forums/models/PostModel.py
@@ -1,28 +1,35 @@
-
+from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+import uuid
+from django.contrib.contenttypes.models import ContentType
 
-from forums.models.ForumModel import *
-from forums.models.ThreadModel import Thread
+from forums.models.CommentModel import Comment
+from forums.models.EngagementModel import DownVote, UpVote
 
 User = get_user_model()
 
-
-class Post(Model):
-
-    id = UUIDField(_("Forum ID"),
-                   primary_key=True,
-                   unique=True,
-                   editable=False,
-                   default=uuid.uuid4)
-    author = ForeignKey(User, on_delete=CASCADE, related_name="thread_posts")
-    thread = ForeignKey(Thread, on_delete=CASCADE)
-    content = TextField(max_length=1000)
-    views = PositiveIntegerField(default=0)
-    likes = PositiveIntegerField(default=0)
-    dislikes = PositiveIntegerField(default=0)
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
+class Post(models.Model):
+    id = models.UUIDField(
+        _("Post ID"),
+        primary_key=True,
+        unique=True,
+        editable=False,
+        default=uuid.uuid4
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="thread_posts"
+    )
+    thread = models.ForeignKey('forums.Thread', on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    views = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = _("Thread Post")
@@ -30,21 +37,13 @@ class Post(Model):
 
     def __str__(self):
         return f"{self.content[:50]}"
-    
+
     def get_absolute_url(self):
         return reverse("ThreadPost_detail", kwargs={"pk": self.pk})
 
     def sliced_content(self):
         return self.content[:40]
 
-    '''
-    def total_upvotes(self):
-        return get_total_upvotes(self, self.id, 'P')
-
-    def total_downvotes(self):
-        return get_total_downvotes(self, self.id, 'P')
-    '''
-    
     def increment_views(self):
         self.views += 1
         self.save()
@@ -59,5 +58,3 @@ class Post(Model):
     def total_post_downvotes(self):
         content_type = ContentType.objects.get_for_model(self)
         return DownVote.objects.filter(content_type=content_type, upvote_type='P', object_id=self.id).count()
-
-
