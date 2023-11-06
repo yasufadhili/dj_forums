fa29443325a6e44b8c13e52f35ee36e040930d61
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-06 10:17:41 +0300

    Update ForumModel.py

--- forums/models/ForumModel.py
+++ forums/models/ForumModel.py
@@ -1,16 +1,4 @@
-from django.db.models import (
-    Model, CharField,
-    TextField,
-    SlugField,
-    DateTimeField,
-    URLField,
-    ForeignKey,
-    CASCADE,
-    ManyToManyField,
-    PositiveIntegerField,
-    UUIDField,
-
-)
+from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -29,34 +17,55 @@ from forums.models.EngagementModel import UpVote, DownVote, get_total_upvotes, g
 User = get_user_model()
 
 
-class Forum(Model):
+class Forum(models.Model):
     ACCESSIBILITY_CHOICES = (
-        ('open', 'Open'),
-        ('subscribed', 'Subscribed')
+        ('open', _('Open')),
+        ('subscribed', _('Subscribed'))
     )
 
     POSTING_PERMISSIONS_CHOICES = (
-        # ('creator', 'Creator'),
-        ('creator_managers', 'Creators & Managers'),
-        ('subscribers', 'Subscribers'),
-        ('everyone', 'Everyone'),
+        ('creator_managers', _('Creators & Managers')),
+        ('subscribers', _('Subscribers')),
+        ('everyone', _('Everyone')),
     )
 
-    id = UUIDField(_("Forum ID"),
-                   primary_key=True,
-                   editable=False,
-                   default=uuid.uuid4)
-    author = ForeignKey(User, on_delete=CASCADE, related_name='forums_created')
-    title = CharField(max_length=255)
-    slug = SlugField(unique=True, blank=True)
-    image = URLField(blank=True, max_length=3000)
-    description = TextField(max_length=1000, blank=True)
-    accessibility = CharField(max_length=50, choices=ACCESSIBILITY_CHOICES, default='open')
-    posting_permissions = CharField(max_length=50, choices=POSTING_PERMISSIONS_CHOICES, default='everyone')
-    managers = ManyToManyField(User, blank=True, related_name='forums_managed')
-    subscribers = ManyToManyField(User, blank=True, related_name='subscribed_forums')
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
+    id = models.UUIDField(
+        _("Forum ID"),
+        primary_key=True,
+        editable=False,
+        default=uuid.uuid4
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='forums_created'
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.URLField(blank=True, max_length=3000)
+    description = models.TextField(max_length=1000, blank=True)
+    accessibility = models.CharField(
+        max_length=50,
+        choices=ACCESSIBILITY_CHOICES,
+        default='open'
+    )
+    posting_permissions = models.CharField(
+        max_length=50,
+        choices=POSTING_PERMISSIONS_CHOICES,
+        default='everyone'
+    )
+    managers = models.ManyToManyField(
+        User,
+        blank=True,
+        related_name='forums_managed'
+    )
+    subscribers = models.ManyToManyField(
+        User,
+        blank=True,
+        related_name='subscribed_forums'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = _("Forum")
@@ -70,20 +79,15 @@ class Forum(Model):
 
     def can_user_post(self, user):
         if self.accessibility == 'subscribed':
-            # If the forum is 'subscribed', check if the user is a subscriber
             return user in self.subscribers.all()
         elif self.posting_permissions == 'everyone':
-            # If the posting permissions are set to 'everyone', anyone can post
             return True
         elif self.posting_permissions == 'creator_managers':
-            # If the posting permissions are set to 'creator & managers',
-            # check if the user is the creator or a designated manager
             return user == self.author or user in self.managers.all()
         else:
-            return False  # Default to not allowing the user to post
+            return False
 
     def save(self, *args, **kwargs):
-        # Generate a unique slug based on title and user's pk
         if not self.slug:
             base_slug = slugify(self.title)
             unique_slug = base_slug
@@ -97,7 +101,6 @@ class Forum(Model):
             self.slug = unique_slug
 
         super(Forum, self).save(*args, **kwargs)
-
     '''
     def total_threads(self):
         threads = Thread.objects.filter(forum=self).count()
@@ -176,21 +179,32 @@ class Forum(Model):
                                        object_id__in=self.threads.values_list('posts__comments__id', flat=True)).count()
 
     def total_engagement(self):
-        return self.total_forum_upvotes() + self.total_forum_downvotes() + \
-            self.total_thread_upvotes() + self.total_thread_downvotes() + \
-            self.total_post_upvotes() + self.total_post_downvotes() + \
-            self.total_comment_upvotes() + self.total_comment_downvotes()
+        forum_upvotes = self.total_forum_upvotes()
+        forum_downvotes = self.total_forum_downvotes()
+        thread_upvotes = self.total_thread_upvotes()
+        thread_downvotes = self.total_thread_downvotes()
+        post_upvotes = self.total_post_upvotes()
+        post_downvotes = self.total_post_downvotes()
+        comment_upvotes = self.total_comment_upvotes()
+        comment_downvotes = self.total_comment_downvotes()
+
+        return (
+                forum_upvotes + forum_downvotes +
+                thread_upvotes + thread_downvotes +
+                post_upvotes + post_downvotes +
+                comment_upvotes + comment_downvotes
+        )
 
 
-class ForumRating(Model):
-    author = ForeignKey(User, on_delete=CASCADE)
-    forum = ForeignKey(Forum, on_delete=CASCADE)
-    rating = PositiveIntegerField(
+class ForumRating(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
 
     class Meta:
-        unique_together = ['author', 'forum']  # Ensure a user can rate a forum only once
+        unique_together = ['author', 'forum']
 
     def __str__(self):
         return f'{self.author.get_username} rated {self.forum.title} with {self.rating} stars'
