28812191a899eb78ac3a54d25dc0169e8c7f781f
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-07 18:15:00 +0300

    Update urls.py

--- forums/urls.py
+++ forums/urls.py
@@ -1,14 +1,39 @@
 from django.urls import path, include
-
-from rest_framework import urls, routers
+from rest_framework.routers import DefaultRouter
 
 from forums.views.ForumView import ForumViewSet
+from forums.views.ThreadView import ThreadViewSet
+from forums.views.PostView import PostViewSet
+from forums.views.CommentView import CommentViewSet
+from forums.views.EngagementView import UpVoteViewSet, DownVoteViewSet
 
-
-router = routers.DefaultRouter()
-router.register(r"", ForumViewSet, basename="forum")
+router = DefaultRouter()
+router.register(r"", ForumViewSet)
+router.register(r"threads", ThreadViewSet)
+router.register(r"posts", PostViewSet)
+router.register(r"comments", CommentViewSet)
+router.register(r"engagements/upvotes", UpVoteViewSet)
+router.register(r"engagements/downvotes", DownVoteViewSet)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("<uuid:forum_id>/", include([
+        path("", ForumViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
+        path("threads/", include([
+            path("", ThreadViewSet.as_view({"get": "list"})),
+            path("<uuid:thread_id>/", include([
+                path("", ThreadViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
+                path("posts/", include([
+                    path("", PostViewSet.as_view({"get": "list"})),
+                    path("<uuid:post_id>/", include([
+                        path("", PostViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
+                        path("comments/", include([
+                            path("", CommentViewSet.as_view({"get": "list"})),
+                            path("<uuid:comment_id>/", CommentViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
+                        ])),
+                    ])),
+                ])),
+            ])),
+        ])),
+    ])),
 ]
-
